1d68f961dcb54fd6c75da17d920e77f9721908c3
Andrei Alvarez <62512410+andreiAlvarez@users.noreply.github.com>
2020-08-16 23:26:13 -0400

    Create number.js

---
+++ number.js
@@ -0,0 +1,16 @@
+function fiboEvenSum(n) {
+  if (n <= 1) {
+    return 0;
+  } else {
+    let nMinus2 = 1, nMinus1 = 2, value = 3, sum = 2;
+    while(value < n) {  
+      if(value % 2 === 0) {
+        sum += value;
+      }
+      nMinus2 = nMinus1;
+      nMinus1 = value;
+      value = nMinus1 + nMinus2;
+    }
+    return sum;
+  }
+}
